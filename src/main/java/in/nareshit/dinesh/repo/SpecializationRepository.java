1184f534a33899a2b47b08471a4ee140a5bab07f
unknown <chaudharidinesh5@gmail.com>
2021-09-17 08:25:12 +0530

    spec repo update

---
+++ src/main/java/in/nareshit/dinesh/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package in.nareshit.dinesh.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nareshit.dinesh.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
